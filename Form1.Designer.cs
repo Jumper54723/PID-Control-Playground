e322bbd3bbb1d71cda50d8f6127f33a8366bd080
Daniel <daniel.l.smith547@outlook.com>
2025-01-06 21:01:08 -0500

    PID functional. Setpoint can be updated and response will track based on error.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.startButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(95, 47);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(135, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(998, 360);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.ClientSize = new System.Drawing.Size(1173, 393);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,72 @@ namespace PID_Control_Playground
         {
             InitializeComponent();
         }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            System.Windows.Forms.DataVisualization.Charting.Series setPointSeries = chart1.Series.Add("Set Point");
+            System.Windows.Forms.DataVisualization.Charting.Series currentValue = chart1.Series.Add("Current Value");
+            setPointSeries.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            currentValue.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            setPointSeries.BorderWidth = 7;
+            currentValue.BorderWidth = 3;
+
+            double outputFlowRate = 2;
+            double inputFlowRate = 0;
+            double currentTankLevel = 100;
+            double setPointLevel = 100;
+            const double ProportionalConstant = 0.2;
+            const double IntegralConstant = 0.0005;
+            const double derivitiveConstant = 0.1;
+
+            //Random random = new Random();
+            //currentValue.Points.AddXY(i, 10 + random.Next(-1, 2));
+
+            List<double> errorPoints = new List<double>();
+            errorPoints.Add(0); // add first point of no error
+            double errorSum = 0;
+            double errorSlope;
+            for (int i = 1; i < 1500; i++)
+            {
+                if(i == 400)
+                {
+                    setPointLevel = 20;
+                }
+                if (i == 800)
+                {
+                    setPointLevel = 170;
+                }
+                setPointSeries.Points.AddXY(i, setPointLevel);
+                currentTankLevel += inputFlowRate - outputFlowRate;
+                // clamp between 0 and 200
+                if(currentTankLevel < 0)
+                {
+                    currentTankLevel = 0;
+                }
+                else if(currentTankLevel > 200)
+                {
+                    currentTankLevel = 200;
+                }
+                currentValue.Points.AddXY(i, currentTankLevel);
+                errorPoints.Add(currentTankLevel - setPointLevel);
+                errorSum += errorPoints[i];
+                errorSlope = errorPoints[i - 1] - errorPoints[i];
+
+                // update inflow rate based on PID every 10th loop
+                if (i % 10 == 0)
+                {
+                    // input flow rate = ProprtionalResponse + IntegralResponse + DerivitiveResponse
+                    inputFlowRate = -1*ProportionalConstant * errorPoints[i] - IntegralConstant*errorSum - derivitiveConstant*errorSlope; // proportional constant multiplied by current error
+                    
+                    // ensure input flow rate can not be less than 0 (we can't start sucking water out of the tower)
+                    if(inputFlowRate < 0)
+                    {
+                        inputFlowRate = 0;
+                    }
+                }
+            }
+
+        }
     }
 }
